63f7e9e4100bfa3f3183c730f939771f328c0fb5
toleressea <danielreid@gmail.com>
2024-11-04 22:24:21 -0500

    Add commuter mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,34 @@ const Home = () => {
   const [actualText, setActualText] = useState<string>("");
   const [status, setStatus] = useState<string>("No text loaded.");
   const [userText, setUserText] = useState<string>("");
+  const [isCommuter, setIsCommuter] = useState<boolean>(false);
+  const filterChars = /[.,\/#!?“”$%\^&\*;:{}=_`~()]/g;
+
+  // const [fail, setFail] = useState<boolean>(false);
+  // const failSound = new Audio('/fail.mp3');
 
   useEffect(() => {
     fetchBible();
   }, []);
 
-  const filterChars = /[.,\/#!?“”$%\^&\*;:{}=_`~()]/g;
+  useEffect(() => {
+    if (isCommuter) {
+      const lc = userText.toLowerCase();
+      if (lc.includes("memory go back")) {
+        revertToActual(false);
+        return;
+      }
+
+      if (lc.includes("memory help")) {
+        revertToActual(true);
+        return;
+      }
+    }
+  }, [userText])
+
+  // useEffect(() => {
+  //   if (fail) failSound.play();
+  // }, [fail])
 
   const fetchBible = async () => {
     try {
@@ -36,7 +58,7 @@ const Home = () => {
       Object.keys(bible).includes(bibleRef.toUpperCase())
     ) {
       const rawText = bible[bibleRef.toUpperCase()];
-      const cleanedText = cleanHTML(rawText);
+      const cleanedText = cleanText(rawText);
 
       setActualText(cleanedText);
       setStatus(`Text from ${bibleRef.toUpperCase()} loaded`);
@@ -61,17 +83,22 @@ const Home = () => {
     }
 
     // split the user input into words
-    let userInputWords = userText.split(" ");
-    userInputWords = userInputWords.map((userWord: string, i: number) => {
-      let actualWord = actualWords[i];
-      if (userWord.toLowerCase() == actualWord.toLowerCase()) {
-        return userWord;
-      } else {
-        return "";
+    let userInputWords = cleanText(userText).split(" ");
+    let i = 0;
+    while (i < userInputWords.length && userInputWords[i].toLowerCase() == actualWords[i].toLowerCase() && i < actualWords.length) {
+      i++;
+    }
+    userInputWords = userInputWords.slice(0, i);
+    
+    if (addNext) {
+      const nextWord = actualWords[userInputWords.length];
+      if (isCommuter) {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = nextWord;
+        window.speechSynthesis.speak(msg);
       }
-    }).filter((word: string) => word != "");
-
-    if (addNext) userInputWords.push(actualWords[userInputWords.length]);
+      userInputWords.push(nextWord);
+    }
 
     setUserText(userInputWords.join(" "));
   }
@@ -90,7 +117,7 @@ const Home = () => {
       : "text-red-500";
   };
 
-  const cleanHTML = (input: string) => {
+  const cleanText = (input: string) => {
     let text = input.replace(/<\/?[^>]+(>|$)/g, ""); // Remove HTML tags
     text = text
       .replace(filterChars, "")
@@ -104,15 +131,19 @@ const Home = () => {
     endOfContentRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [userText]);
 
+  const handleCommuterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsCommuter(event.target.checked);
+  };
+
   return (
-    <div
-      className="p-4 bg-blue-100 rounded-lg shadow-md m-auto max-w-xl"
-      style={{ marginTop: "16px" }}
-    >
-      <h3 className="p-4" style={{ color: "black", textAlign: "center" }}>
-        NKJV Bible Memory Helper
-      </h3>
-      <span style={{ whiteSpace: "pre-line", color: "black" }}>
+      <div
+          className="p-4 bg-blue-100 rounded-lg shadow-md m-auto max-w-xl"
+          style={{ marginTop: "16px" }}
+      >
+        <h3 className="p-4" style={{ color: "black", textAlign: "center" }}>
+          NKJV Bible Memory Helper
+        </h3>
+        <span style={{ whiteSpace: "pre-line", color: "black" }}>
         1. Enter a book and chapter to work on, e.g. James 1 <br />
         2. Start typing from memory
         <br />
@@ -122,90 +153,113 @@ const Home = () => {
         - The text changes color to grade success
         <br />- Ignores most punctuation / capitalization
         <br />- Press Enter key for help with current / next word
-      </span>
-      <input
-        type="text"
-        value={bibleRef}
-        onChange={(e: ChangeEvent<HTMLInputElement>) =>
-          setBibleRef(e.target.value)
-        }
-        className="w-full p-2 mb-4 rounded border border-blue-300 text-black"
-        style={{ marginTop: "16px" }}
-        placeholder="Enter Bible reference..."
-      />
+        </span>
+        <input
+            type="text"
+            value={bibleRef}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setBibleRef(e.target.value)
+            }
+            className="w-full p-2 mb-4 rounded border border-blue-300 text-black"
+            style={{ marginTop: "16px" }}
+            placeholder="Enter Bible reference..."
+        />
 
-      <button
-        onClick={fetchChapter}
-        className="w-full p-2 mb-4 rounded bg-blue-500 text-white"
-      >
-        Load
-      </button>
-
-      {actualText && (
-        <>
-          <button
-            onClick={populateActual}
+        <button
+            onClick={fetchChapter}
             className="w-full p-2 mb-4 rounded bg-blue-500 text-white"
-          >
-            See Full Text
-          </button>
-          <textarea
-            onKeyDown={(event) => {
-              if (event.key === "Enter") {
-                event.preventDefault();
-                revertToActual(true);
-              }
+        >
+          Load
+        </button>
+
+        {actualText && (
+            <>
+              <button
+                  onClick={populateActual}
+                  className="w-full p-2 mb-4 rounded bg-blue-500 text-white"
+              >
+                See Full Text
+              </button>
+              <textarea
+                  onKeyDown={(event) => {
+                    if (event.key === "Enter") {
+                      event.preventDefault();
+                      revertToActual(true);
+                    }
+                  }}
+                  value={userText}
+                  onChange={checkText}
+                  className="w-full py-2 px-2 mb-0 rounded border border-blue-300 text-black"
+                  placeholder="Type from memory..."
+              />
+            </>
+        )}
+
+        <hr
+            style={{
+              border: "none",
+              height: "2px",
+              backgroundColor: "black",
+              marginTop: "8px",
+              marginBottom: "8px",
             }}
-            value={userText}
-            onChange={checkText}
-            className="w-full py-2 px-2 mb-0 rounded border border-blue-300 text-black"
-            placeholder="Type from memory..."
-          />
-        </>
-      )}
+        />
 
-      <hr
-        style={{
-          border: "none",
-          height: "2px",
-          backgroundColor: "black",
-          marginTop: "8px",
-          marginBottom: "8px",
-        }}
-      />
-
-      {actualText && (
-        <>
-          <div className="space-x-1 overflow-auto max-h-64">
-            {cleanHTML(userText)
-              .split(" ")
-              .map((word, wordIndex) => {
-                return (
-                  <span
-                    key={wordIndex}
-                    className={"inline-block " + colorWord(word, wordIndex)}
-                  >
+        {actualText && (
+            <>
+              <div className="space-x-1 overflow-auto max-h-64">
+                {cleanText(userText)
+                    .split(" ")
+                    .map((word, wordIndex) => {
+                      return (
+                          <span
+                              key={wordIndex}
+                              className={"inline-block " + colorWord(word, wordIndex)}
+                          >
                     {word}
                   </span>
-                );
-              })}
-            <div ref={endOfContentRef} />
-          </div>
-        </>
-      )}
+                      );
+                    })}
+                <div ref={endOfContentRef} />
+              </div>
+            </>
+        )}
 
-      <hr
-        style={{
-          border: "none",
-          height: "2px",
-          backgroundColor: "black",
-          marginTop: "8px",
-        }}
-      />
-      <div style={{ textAlign: "center", marginTop: "8px" }}>
-        <span style={{ color: "black" }}>{status}</span>
+        <hr
+            style={{
+              border: "none",
+              height: "2px",
+              backgroundColor: "black",
+              marginTop: "8px",
+            }}
+        />
+        <div style={{ textAlign: "center", marginTop: "8px" }}>
+          <span style={{ color: "black" }}>{status}</span>
+        </div>
+
+        <hr
+            style={{
+              border: "none",
+              height: "2px",
+              backgroundColor: "black",
+              marginTop: "8px",
+            }}
+        />
+
+        <label style={{ color: "black" }}>
+          <input
+              type="checkbox"
+              checked={isCommuter}
+              onChange={handleCommuterChange}
+          />
+          {" "}Commuter
+        </label>
+        <span style={{ whiteSpace: "pre-line", color: "black" }}>
+        <br />Check the box if you're commuting to gain access to the following voice commands:
+        <br />- "memory go back" to undo a mistake
+        <br />- "memory help" to add the next word
+        </span>
       </div>
-    </div>
   );
 };
 
